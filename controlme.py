d190b6cb0419543a57a1b8be3a915255879b43a6
Reid <reidwicks@hotmail.com>
2017-05-09 01:42:08 +1200

    Changed command from if/elif tree to a dictionary, as it's the closest thing Python has to a switch.
    New project goal: detecting button presses that are more than just binary.

--- controlme.py
+++ controlme.py
@@ -1,13 +1,27 @@
-#!usr/bin/bash
+#!usr/bin/python
+
 import fileinput
+
+buttons = {"du:1" : "Up",
+           "dd:1" : "Down",
+           "dl:1" : "Left",
+           "dr:1" : "Right",
+           "back:1" : "Back",
+           "guide:1" : "Guide",
+           "start:1" : "Start",
+           "TL:1" : "Left joystick button",
+           "TR:1" : "Right joystick button",
+           "A:1" : "A",
+           "B:1" : "B",
+           "X:1" : "X",
+           "Y:1" : "Y",
+           "LB:1" : "Left bumper",
+           "RB:1" : "Right bumper",
+           "LT:255" : "Left trigger (fully depressed)",
+           "RT:255" : "Right trigger (fully depressed)",
+           }
 
 for line in fileinput.input():
     for item in line.split():
-        if item == "du:1":
-            print("Up")
-        elif item == "dd:1":
-            print("Down")
-        elif item == "dl:1":
-            print("Left")
-        elif item == "dr:1":
-            print("Right")
+        if item in buttons:
+            print(buttons[item])
